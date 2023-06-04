80e313200de0eccc48d569b2e4cf7f3b5f7a4211
sanjay3291 <35068895+sanjay3291@users.noreply.github.com>
2023-06-04 23:08:57 +0000

    Added ENV KEY to bot and start message.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-First commit
+// imports the node package for us
+const TelegramBot = require('node-telegram-bot-api');
+
+// grabs the environment variable
+const token = process.env.TELEGRAM_BOT_KEY;
+
+// starts a new Telegram bot instance that "polls" for updates
+const bot = new TelegramBot(token, {polling: true});
+
+// send the following text on starting the bot
+bot.onText(/\/start/, (msg) => {
+// listens for "/start" and responds with the greeting below.
+bot.sendMessage(msg.chat.id,
+"Hey, I'm the Deepa Quote Bot, I send motivational quotes similar to what Deepa says in real life !!");
+});
